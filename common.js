f83cade233d9eca9741527f0004ce96d5314c89b
Adam Haertter <80988984+adamhaertter@users.noreply.github.com>
2021-06-22 13:57:29 -0400

    Support for smash dataset

--- common.js
+++ common.js
@@ -12,17 +12,22 @@ function generate(){
 	}
 	
 function toggle(theDiv) {
-		var charName = theDiv.textContent;
-		var divBox = document.querySelectorAll('input[ class="char_boxes"][name='+charName+']');
+		var charName = theDiv.name;
+		try {
+		var divBox = document.querySelectorAll('input[class="char_boxes"][name="'+charName+'"]');
+		} catch(e) {
+			console.log("Problem enountered while querying for "+charName);
+		}
 		divBox = divBox.item(0);
 		if(divBox.checked == true) {
 			theDiv.style.backgroundColor = "#ACACAC";
 			divBox.checked = false;
 		} else {
-			theDiv.style.backgroundColor = "greenyellow";
+			theDiv.style.backgroundColor = "#39e6a6";
 			divBox.checked = true;
 		}
 	}
+
 		
 window.onload = function loadIcons(){
 		var iconImages = document.querySelectorAll('img[class="char_icons"]');
