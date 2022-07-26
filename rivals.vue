3c16183881fe7856f1100896154e0b601d300936
Adam Haertter <adamhaertter@gmail.com>
2022-07-26 17:02:33 -0400

    Add support for Rivals dataset

---
+++ rivals.vue
@@ -0,0 +1,26 @@
+const app = new Vue({
+		el: '#app',
+		data: {
+			characters: [
+				'Zetterburn', 
+				'Orcane',
+				'Wrastor', 
+				'Kragg', 
+				'Forsburn', 
+				'Maypul', 
+				'Absa', 
+				'Etalus', 
+				'Ranno', 
+				'Clairen', 
+				'Sylvanos', 
+				'Elliana', 
+				'Ori and Sein', 
+				'Shovel Knight', 
+				'Mollo', 
+				'Hodan', 
+				'Pomme', 
+				'Olympia', 
+				'Workshop Char'
+			]
+		}
+	})
