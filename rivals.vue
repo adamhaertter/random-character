569c641dcb484e2b509da06c7ab5a990e4bc4a16
Adam Haertter <adamhaertter@gmail.com>
2022-07-26 17:38:05 -0400

    Add maps for Rivals dataset

--- rivals.vue
+++ rivals.vue
@@ -21,6 +21,25 @@ const app = new Vue({
 				'Pomme', 
 				'Olympia', 
 				'Workshop Char'
+			],
+			
+			maps: [
+				'(Neo) Fire Capital',
+				'Air Armada, Diamond Grove',
+				'The Rock Wall, The RoA Ring',
+				'Merchant Port, The Swampy Estuary',
+				'Treetop Lodge, Highdive Hideaway',
+				'Blazing Hideout, Neo Blazing Rail',
+				'Tempest Peak',
+				'Frozen Fortress',
+				'Tower of Heaven, Aether High',
+				'Aethereal Gates, Frozen Gates',
+				'The Endless Abyss',
+				'The Spirit Tree',
+				'The Forest Floor, Crystal Oasis',
+				'(Neo) Julesvale',
+				'Troupple Pond, Pridemoor Keep',
+				'Workshop Stage'
 			]
 		}
 	})
